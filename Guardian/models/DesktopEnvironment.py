ee0a4a055f27778b865842a349552fd0472ba8a8
pratap38 <pratapchoubey3@gmail.com>
2026-08-02 03:50:18 +0530

    Ram monitor system

---
+++ Guardian/models/DesktopEnvironment.py
@@ -0,0 +1,26 @@
+from enum import Enum,auto
+
+class DesktopEnv(Enum):
+    x11=auto()
+    WAYLAND=auto()
+    UNKNOWN=auto()
+    def __str__(self):
+        if self==DesktopEnv.x11:
+            return "X11"
+        elif self==DesktopEnv.WAYLAND:
+            return "Wayland"
+        return "UnKnown"
+    @property
+    def Description(self):
+        description={
+            DesktopEnv.x11:
+            "Traditional Linux display server",
+            DesktopEnv.WAYLAND:
+            "Mordern  secure Linux  display protocol",
+            DesktopEnv.UNKNOWN:
+            "Desktop env  not been detected"
+        }
+        return description[self]
+    @property
+    def supportwindowTrack(self):
+        return self==DesktopEnv.x11
