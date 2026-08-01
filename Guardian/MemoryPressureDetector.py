824c724f4b7cbffb4c19abf0f34427a9de37be2f
pratap38 <pratapchoubey3@gmail.com>
2026-08-02 03:02:13 +0530

    rammemorydetection

--- Guardian/MemoryPressureDetector.py
+++ Guardian/MemoryPressureDetector.py
@@ -1,5 +1,40 @@
+from Guardian.GuardianConfig import GuardianConfig
+from models.PressureState import PressureState
+from models.MemoryInfo import MemoryInfo
 
 
-class MemoryPressureDetector:
-    def analyze(memoryInfo)->PressureState:
-        pass
+class MemoryPressureCheck:
+    def __init__(self):
+        self.config=GuardianConfig()
+
+        def analyze(
+                self,
+                memory:MemoryInfo
+        )->PressureState:
+            ramUsage=memory.ramPercent
+            emergency=self.config.get(
+                "emergencyThreshold"
+            )
+            critical=self.config.get(
+                "criticalThreshold"
+            )
+            warning=self.config.get(
+                "warningThreshold"
+            )
+            if ramUsage>=emergency:
+                return PressureState.EMERGENCY
+            elif ramUsage>=critical:
+                return PressureState.CRITICAL
+            elif ramUsage>=warning:
+                return PressureState.WARNING
+            return PressureState.NORMAL
+        def isNormal(self,memory:MemoryInfo)->bool:
+            return(self.analyze(memory)==PressureState.NORMAL)
+        def isWarning(self,memory:MemoryInfo)->bool:
+            return(self.analyze(memory)==PressureState.WARNING)
+        def isCritical(self,memory:MemoryInfo)->bool:
+                    return(self.analyze(memory)==PressureState.CRITICAL)
+        def isEmergency(self,memory:MemoryInfo)->bool:
+                    return(self.analyze(memory)==PressureState.EMERGENCY)
+        
+                
