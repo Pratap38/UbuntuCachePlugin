f7b0c3cbbc5f585b8a53372c55e828c80f9c8178
pratap38 <pratapchoubey3@gmail.com>
2026-08-23 17:01:26 +0530

    model to store day which process is been pause

---
+++ Guardian/models/PausedProcess.py
@@ -0,0 +1,28 @@
+from dataclasses import  dataclass
+from datetime import datetime
+
+@dataclass(slots=True)
+class PauseProcess:
+    pid: int
+    name: str
+    pausedAt: datetime
+    reason: str
+
+    def __str__(self)->str:
+        return (
+            f"PausedProcess("
+            f"pid={self.pid}, "
+            f"name={self.name}, "
+            f"reason={self.reason}, "
+            f"pausedAt={self.pausedAt}"
+            f")"
+        )
+    def toDict(self) -> dict:
+
+        return {
+            "pid": self.pid,
+            "name": self.name,
+            "paused_at": self.pausedAt.isoformat(),
+            "reason": self.reason,
+        }
+        
