dfcddc087373607cd36da55494c5a8fc707d1662
pratap38 <pratapchoubey3@gmail.com>
2026-08-29 00:57:17 +0530

    added select specific candidate in orchestrator

--- Guardian/GuardianOrchestrator.py
+++ Guardian/GuardianOrchestrator.py
@@ -24,8 +24,18 @@ class GuardianOrchestrator:
         self.resumePolicy=ResumePolicy()
         self.eventHistory=EventHistory()
         self.notificationManager=NotificationManager()
+
+#scan the machine memory info
     def analyzeMemory(self):
         memory=self.ramMonitor.collect()
         pressure=self.pressureCheck.analyze(memory)
         decision=self.decisionEngine.decide(pressure)
         return memory,pressure,decision
+## select the specific candidate in order to take the decision action
+    def getCandidate(self):
+        memory,pressure,decision=self.analyzeMemory()
+        if not decision:
+            return memory,pressure,decision,[]
+        candidate=self.candidateSelector.getCandidates()
+
+        return memory,pressure,decision,candidate
