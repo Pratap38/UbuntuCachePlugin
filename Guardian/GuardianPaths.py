1d00a0122b2b03fd059c2822d444b4641868af11
pratap38 <pratapchoubey3@gmail.com>
2026-09-10 23:17:30 +0530

    centralise path directory created

---
+++ Guardian/GuardianPaths.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+
+
+class GuardianPaths:
+
+    APP_NAME = "ubuntu-cache-cleaner"
+
+    @classmethod
+    def homeDirectory(cls) -> Path:
+        return Path.home()
+
+    @classmethod
+    def configDirectory(cls) -> Path:
+        return (
+            cls.homeDirectory()
+            / ".config"
+            / cls.APP_NAME
+        )
+
+    @classmethod
+    def configFile(cls) -> Path:
+        return (
+            cls.configDirectory()
+            / "guardian_config.json"
+        )
+
+    @classmethod
+    def stateDirectory(cls) -> Path:
+        return (
+            cls.homeDirectory()
+            / ".local"
+            / "state"
+            / cls.APP_NAME
+        )
+
+    @classmethod
+    def stateFile(cls) -> Path:
+        return (
+            cls.stateDirectory()
+            / "guardian_state.json"
+        )
+
+    @classmethod
+    def logDirectory(cls) -> Path:
+        return (
+            cls.stateDirectory()
+            / "logs"
+        )
+
+    @classmethod
+    def logFile(cls) -> Path:
+        return (
+            cls.logDirectory()
+            / "guardian.log"
+        )
