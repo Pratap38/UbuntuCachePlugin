213351df3b7ec5cd34e7885b160fde1d65218c9c
pratap38 <pratapchoubey3@gmail.com>
2026-09-14 23:05:59 +0530

    implemented the lock system even the guardian crashes using fcntl

--- Guardian/GuardianDaemon.py
+++ Guardian/GuardianDaemon.py
@@ -1,21 +1,96 @@
+# import signal
+
+# from Guardian.GuardianEngine import GuardianEngine
+
+
+
+# ##we will use this command first in order to stop SIGTERM → normal systemd stop
+# # SIGINT  → Ctrl+C / manual interruption
+# def main():
+
+#     engine=GuardianEngine()
+
+#     def handleShutdown(signum,frame):
+#         engine.stop()
+#     signal.signal(signal.SIGTERM,handleShutdown)
+#     signal.signal(signal.SIGINT,handleShutdown)
+
+#     engine.start()
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+import fcntl
 import signal
 
 from Guardian.GuardianEngine import GuardianEngine
+from Guardian.GuardianPaths import GuardianPaths
 
 
-
-##we will use this command first in order to stop SIGTERM → normal systemd stop
-# SIGINT  → Ctrl+C / manual interruption
 def main():
 
-    engine=GuardianEngine()
+    lockFile = GuardianPaths.stateDirectory() / "guardian.lock"
 
-    def handleShutdown(signum,frame):
-        engine.stop()
-    signal.signal(signal.SIGTERM,handleShutdown)
-    signal.signal(signal.SIGINT,handleShutdown)
+    lockFile.parent.mkdir(
+        parents=True,
+        exist_ok=True
+    )
 
-    engine.start()
+    lockHandle = open(
+        lockFile,
+        "w",
+        encoding="utf-8"
+    )
+
+    try:
+
+        try:
+
+            fcntl.flock(
+                lockHandle.fileno(),
+                fcntl.LOCK_EX | fcntl.LOCK_NB
+            )
+
+        except BlockingIOError:
+
+            print(
+                "RAM Guardian is already running."
+            )
+
+            return
+
+        engine = GuardianEngine()
+
+        def handleShutdown(signum, frame):
+            engine.stop()
+
+        signal.signal(
+            signal.SIGTERM,
+            handleShutdown
+        )
+
+        signal.signal(
+            signal.SIGINT,
+            handleShutdown
+        )
+
+        engine.start()
+
+    finally:
+
+        try:
+
+            fcntl.flock(
+                lockHandle.fileno(),
+                fcntl.LOCK_UN
+            )
+
+        finally:
+
+            lockHandle.close()
+
 
 if __name__ == "__main__":
     main()
